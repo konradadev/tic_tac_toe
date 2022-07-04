d3283ece3e3c4b407feb0bbe44a8699bf6036db6
konradadev <konradadev@gmail.com>
2022-07-05 01:12:04 +0200

    Add display_board method in Game class

---
+++ script.rb
@@ -0,0 +1,19 @@
+class Game
+  @@board = {
+    a: ['   ', '   ', '   '],
+    b: ['   ', '   ', '   '],
+    c: ['   ', '   ', '   ']
+  }
+
+  def display_board
+    puts ('   1   2   3')
+    puts "A #{@@board[:a][0]}|#{@@board[:a][1]}|#{@@board[:a][2]}"
+    puts ('  -----------')
+    puts "B #{@@board[:b][0]}|#{@@board[:b][1]}|#{@@board[:b][2]}"
+    puts ('  -----------')
+    puts "C #{@@board[:c][0]}|#{@@board[:c][1]}|#{@@board[:c][2]}"
+  end
+end
+
+board1 = Game.new
+board1.display_board
