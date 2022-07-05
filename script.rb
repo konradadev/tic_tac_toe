f535da4fd8a16d9ed9e541ec72b3f5cbbb30aba3
konradadev <konradadev@gmail.com>
2022-07-05 13:21:15 +0200

    Add clear_board function

--- script.rb
+++ script.rb
@@ -1,16 +1,20 @@
 class Game
-  @@board = {
-    a: ['   ', '   ', '   '],
-    b: ['   ', '   ', '   '],
-    c: ['   ', '   ', '   ']
-  }
+  @@board = {}
+
+  def clear_board
+    @@board = {
+      a: ['   ', '   ', '   '],
+      b: ['   ', '   ', '   '],
+      c: ['   ', '   ', '   ']
+    }
+  end
 
   def display_board
-    puts ('   1   2   3')
+    puts '   1   2   3'
     puts "A #{@@board[:a][0]}|#{@@board[:a][1]}|#{@@board[:a][2]}"
-    puts ('  -----------')
+    puts '  -----------'
     puts "B #{@@board[:b][0]}|#{@@board[:b][1]}|#{@@board[:b][2]}"
-    puts ('  -----------')
+    puts '  -----------'
     puts "C #{@@board[:c][0]}|#{@@board[:c][1]}|#{@@board[:c][2]}"
   end
 end
