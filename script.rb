95d5d42d815d9aa482be3055ec75a685c9a639d1
konradadev <konradadev@gmail.com>
2022-07-05 13:23:55 +0200

    Add initialize method to Game class

--- script.rb
+++ script.rb
@@ -1,6 +1,11 @@
 class Game
   @@board = {}
 
+  def initialize(player1, player2)
+    @player1 = ' ' << player1 << ' '
+    @player2 = ' ' << player2 << ' '
+  end
+
   def clear_board
     @@board = {
       a: ['   ', '   ', '   '],
