0768171fcd2f217bd95a22630bc69803331487ee
konradadev <konradadev@gmail.com>
2022-07-05 16:54:54 +0200

    Fix a typo  in display_board function

--- script.rb
+++ script.rb
@@ -16,11 +16,11 @@ class Game
 
   def display_board
     puts '   1   2   3'
-    puts "A #{@@board[:a][0]}|#{@@board[:a][1]}|#{@@board[:a][2]}"
+    puts "A #{@board[:a][0]}|#{@board[:a][1]}|#{@board[:a][2]}"
     puts '  -----------'
-    puts "B #{@@board[:b][0]}|#{@@board[:b][1]}|#{@@board[:b][2]}"
+    puts "B #{@board[:b][0]}|#{@board[:b][1]}|#{@board[:b][2]}"
     puts '  -----------'
-    puts "C #{@@board[:c][0]}|#{@@board[:c][1]}|#{@@board[:c][2]}"
+    puts "C #{@board[:c][0]}|#{@board[:c][1]}|#{@board[:c][2]}"
   end
 
   def player_move(row, column, player)
