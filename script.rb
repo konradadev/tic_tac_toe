cdfa922bab004a9bee04d37a6126f2912505b7fc
konradadev <konradadev@gmail.com>
2022-07-07 12:06:42 +0200

    Add game_won? function

--- script.rb
+++ script.rb
@@ -26,6 +26,36 @@ class Game
   def player_move(row, column, player)
     @board[row.to_sym][column - 1] = player
   end
+
+  def game_won?(player)
+    game_won = false
+    @board.each { |value| game_won = true if value.all?(player) }
+    for i in 0..2 do
+      column = []
+      @board.each_key { |key| column.push(@board[key][i]) }
+      game_won = true if column.all?(player)
+    end
+    @board.each_key do |key|
+      diagonal = []
+      for i in 0..2 do
+        diagonal.push(@board[key][i])
+      end
+      game_won = true if diagonal.all?(player)
+      alt_diagonal = []
+      j = 2
+      while j >= 0
+        alt_diagonal.push(@board[key][j])
+        j -= 1
+      end
+      game_won = true if alt_diagonal.all?(player)
+    end
+    display_win(player) if game_won == true
+  end
+
+  def display_win(player)
+    display_board()
+    puts "Player #{player} won"
+  end
 end
 
 board1 = Game.new('X', 'O')
